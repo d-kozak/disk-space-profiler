4b8d684bafa2035b2baa18919ca98a06586d7661
dkozak <dkozak94@gmail.com>
2020-01-22 21:29:41 +0100

    scanner: refactor fs node comparators from manually written to declarative style

--- scanner/src/main/kotlin/io/dkozak/profiler/scanner/fs/FsNodeComparators.kt
+++ scanner/src/main/kotlin/io/dkozak/profiler/scanner/fs/FsNodeComparators.kt
@@ -1,27 +1,28 @@
 package io.dkozak.profiler.scanner.fs
 
-import io.dkozak.profiler.scanner.util.Cleanup
 import javafx.scene.control.TreeItem
 
 /**
+ * Simple comparator putting directories before files
+ */
+val FsNodeDirectoryBeforeFileComparator = Comparator<TreeItem<FsNode>> { left, right ->
+    when {
+        left.value is FsNode.DirectoryNode && right.value !is FsNode.DirectoryNode -> -1
+        left.value !is FsNode.DirectoryNode && right.value is FsNode.DirectoryNode -> 1
+        else -> 0
+    }
+}
+
+/**
  * Sort criteria(descending priority):
  * directories before files
  * size - descending
  * name - ascending
  */
-@Cleanup("Use fluent comparator builders instead")
-object FsNodeBySizeComparator : Comparator<TreeItem<FsNode>> {
+val FsNodeBySizeComparator = FsNodeDirectoryBeforeFileComparator
+        .thenByDescending { it.value.size }
+        .thenBy { it.file.name.toUpperCase() }
 
-    override fun compare(left: TreeItem<FsNode>, right: TreeItem<FsNode>): Int = when {
-        left.value is FsNode.DirectoryNode && right.value !is FsNode.DirectoryNode -> -1
-        left.value !is FsNode.DirectoryNode && right.value is FsNode.DirectoryNode -> 1
-        else -> {
-            val cmp = right.value.size.compareTo(left.value.size)
-            if (cmp != 0) cmp
-            else left.value.file.name.compareTo(right.value.file.name)
-        }
-    }
-}
 
 /**
  * Sort criteria(descending priority):
@@ -29,16 +30,6 @@ object FsNodeBySizeComparator : Comparator<TreeItem<FsNode>> {
  * name - ascending
  * size - descending
  */
-@Cleanup("Use fluent comparator builders instead")
-object FsNodeByNameComparator : Comparator<TreeItem<FsNode>> {
-
-    override fun compare(left: TreeItem<FsNode>, right: TreeItem<FsNode>): Int = when {
-        left.value is FsNode.DirectoryNode && right.value !is FsNode.DirectoryNode -> -1
-        left.value !is FsNode.DirectoryNode && right.value is FsNode.DirectoryNode -> 1
-        else -> {
-            val cmp = left.value.file.name.compareTo(right.value.file.name)
-            if (cmp != 0) cmp
-            else right.value.size.compareTo(left.value.size)
-        }
-    }
-}
+val FsNodeByNameComparator = FsNodeDirectoryBeforeFileComparator
+        .thenBy { it.file.name.toUpperCase() }
+        .thenByDescending { it.value.size }
